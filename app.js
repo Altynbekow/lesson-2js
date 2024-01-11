a33f8e3070ccd5548f3e4a84570303c148855f85
Altynbekow <152465429+Altynbekow@users.noreply.github.com>
2024-01-11 10:47:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// log3();
+// console.log(1);
+//
+// function log3(){
+//     console.log(3);
+// }
+// console.log(2);
+
+console.log("hi");
+
+setTimeout(function timeout(){
+    console.log("click the button");
+}, 5000)
+
+console.log("welcome to loupe")
+
+
+
+
+//sync
+// log
+// for loop
+// const let
+// if else
+// function
+
+//async
+// setTimeout()
+// setInterval()
+// promise
+// async/await
+// onclick,onchange,oninput
